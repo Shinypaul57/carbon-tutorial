32f9de293910d3cb8b78d3f1e338fea0983ab734
Shinypaul57 <shinyj.paul@gmail.com>
2023-07-20 00:59:45 +0530

    feat(tutorial): complete step 1

---
+++ src/components/TutorialHeader/TutorialHeader.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Switcher, Notification, UserAvatar } from '@carbon/react/icons';
+import { Link } from 'react-router-dom';
+
+import {
+  Header,
+  HeaderContainer,
+  HeaderName,
+  HeaderNavigation,
+  HeaderMenuButton,
+  HeaderMenuItem,
+  HeaderGlobalBar,
+  HeaderGlobalAction,
+  SkipToContent,
+  SideNav,
+  SideNavItems,
+  HeaderSideNavItems,
+} from '@carbon/react';
+
+const TutorialHeader = () => (
+  <HeaderContainer
+    render={({ isSideNavExpanded, onClickSideNavExpand }) => (
+      <Header aria-label="Carbon Tutorial">
+        <SkipToContent />
+        <HeaderMenuButton
+          aria-label="Open menu"
+          onClick={onClickSideNavExpand}
+          isActive={isSideNavExpanded}
+        />
+        <HeaderName as={Link} to="/" prefix="TS">
+          MidnightRain
+        </HeaderName>
+        <HeaderNavigation aria-label="Carbon Tutorial">
+          <HeaderMenuItem as={Link} to="/repos">
+            Repositories
+          </HeaderMenuItem>
+          <HeaderMenuItem href="/taylor">Artist</HeaderMenuItem>
+        </HeaderNavigation>
+        <SideNav
+          aria-label="Side navigation"
+          expanded={isSideNavExpanded}
+          isPersistent={false}>
+          <SideNavItems>
+            <HeaderSideNavItems>
+              <HeaderMenuItem as={Link} to="/repos">
+                Repositories
+              </HeaderMenuItem>
+              <HeaderMenuItem href="/taylor">Artist</HeaderMenuItem>
+            </HeaderSideNavItems>
+          </SideNavItems>
+        </SideNav>
+        <HeaderGlobalBar>
+          <HeaderGlobalAction
+            aria-label="Notifications"
+            tooltipAlignment="center">
+            <Notification size={20} />
+          </HeaderGlobalAction>
+          <HeaderGlobalAction
+            aria-label="User Avatar"
+            tooltipAlignment="center">
+            <UserAvatar size={20} />
+          </HeaderGlobalAction>
+          <HeaderGlobalAction aria-label="App Switcher" tooltipAlignment="end">
+            <Switcher size={20} />
+          </HeaderGlobalAction>
+        </HeaderGlobalBar>
+      </Header>
+    )}
+  />
+);
+
+export default TutorialHeader;
